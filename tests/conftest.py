11ac15c78ddc1b09845e2b7b777606e1817bd36e
Gluyki <betonbar@bk.ru>
2026-04-23 18:54:47 +0300

    Реализован функционал class category.py, class products.py. Сделаны тесты Реализована функция выгрузки из json файла в модуле utils.py

---
+++ tests/conftest.py
@@ -0,0 +1,39 @@
+import pytest
+
+from src.products import Product
+from src.category import Category
+
+
+@pytest.fixture
+def fix_product():
+    return Product(
+    name ='QLED 4K',
+    description = 'Фоновая подсветка',
+    price = 100000,
+    quantity = 101
+    )
+
+@pytest.fixture
+def fix_category_1():
+    return Category(
+        name = 'Смартфоны',
+        description = 'Смартфоны, как средство',
+        products = [Product(
+            name='QLED 4K',
+            description='Фоновая подсветка',
+            price=100000,
+            quantity=101
+        )]
+    )
+
+@pytest.fixture
+def fix_category_2():
+    return Category(
+        name = 'Телевизоры',
+        description = 'Современный телевизор',
+        products = [Product(
+            "55\" QLED 4K",
+            "Фоновая подсветка",
+            123000.0,
+            7)]
+    )
---
+++ tests/test_category.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+def test_category_init(fix_category_1, fix_category_2):
+    assert fix_category_1.name == 'Смартфоны'
+    assert fix_category_1.description == 'Смартфоны, как средство'
+
+    assert fix_category_2.name == 'Телевизоры'
+    assert fix_category_2.description == 'Современный телевизор'
+
+    assert fix_category_1.category_count == 2
+    assert fix_category_2.category_count == 2
+    assert fix_category_1.product_count == 2
+    assert fix_category_2.product_count == 2
